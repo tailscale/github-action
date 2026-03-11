48c6d0317a1f8ac4c13c0679c2f17adc61ddc827
Mario Minardi <mario@tailscale.com>
2026-03-11 09:22:00 -0600

    action,dist,src: bump default version to 1.94.2
    Bump default version used by the action to 1.94.2. This will resolve
    warnings in the admin console about known security vulnerabilities in
    the previous default of 1.90.4 which aren't applicable for the GitHub
    action.
    
    The security vulnerabilities are specifically related to MDM policies
    for MacOS (see https://tailscale.com/security-bulletins#ts-2026-001) and
    related to using Tailnet lock without passing `--statedir` or `--state`
    to tailscaled (see https://tailscale.com/security-bulletins#ts-2025-008).
    
    Neither of these impact the GitHub action as MacOS runners aren't
    managed by MDM policies and we pass either `--statedir` or `--state` to
    the tailscale daemon.
    
    Fixes https://github.com/tailscale/github-action/issues/272
    
    Signed-off-by: Mario Minardi <mario@tailscale.com>

--- dist/index.js
+++ dist/index.js
@@ -52635,7 +52635,7 @@ async function getInputs() {
         }
     }
     const config = {
-        version: core.getInput("version") || "1.90.4",
+        version: core.getInput("version") || "1.94.2",
         resolvedVersion: "",
         arch: "",
         authKey: authKey,
